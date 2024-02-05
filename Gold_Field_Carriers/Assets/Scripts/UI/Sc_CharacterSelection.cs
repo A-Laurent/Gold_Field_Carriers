101a773b418cc85364911073bf6feeb507a0b450
Roux Cédric <c.roux@lahorde.school>
2024-02-05 11:48:08 +0100

    Character Selecton
    - change selected character
    - add name

--- Gold_Field_Carriers/Assets/Scripts/UI/Sc_CharacterSelection.cs
+++ Gold_Field_Carriers/Assets/Scripts/UI/Sc_CharacterSelection.cs
@@ -1,36 +1,119 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class Sc_CharacterSelection : MonoBehaviour
 {
     [SerializeField] private Color32 _characterAlreadySelectedColor;
+    private Sc_SpritesCharacters _spritesCharacters;
+
+    private bool _firstSelection = false;
+    private bool _secondSelection = false;
+    private bool _thirdSelection = false;
+
+    private void Start()
+    {
+        _spritesCharacters = Sc_SpritesCharacters.Instance;
+    }
+
+    public void UndoFirstCharacter()
+    {
+        if (_spritesCharacters._selectedCharacters[0] != null)
+        {
+            this.GetComponentInChildren<Button>().interactable = true;
+            _spritesCharacters._selectCharacterButton[1].interactable = false;
+            _spritesCharacters._selectCharacterButton[2].interactable = false;
+            this.gameObject.transform.GetChild(0).GetChild(1).GetComponent<Button>().interactable = false;
+            this.GetComponent<Image>().sprite = null;
+
+            _spritesCharacters._selectedCharacters[0].GetComponent<Sc_CanSelect>()._canSelect = true;
+            _spritesCharacters._selectedCharacters[0].GetComponent<Image>().color = Color.white;
+            _spritesCharacters._selectedCharacters[0] = null;
+        }
+    }
+
+    public void UndoSecondCharacter()
+    {
+        if (_spritesCharacters._selectedCharacters[1] != null)
+        {
+            this.GetComponentInChildren<Button>().interactable = true;
+            _spritesCharacters._selectCharacterButton[0].interactable = false;
+            _spritesCharacters._selectCharacterButton[2].interactable = false;
+            this.gameObject.transform.GetChild(0).GetChild(1).GetComponent<Button>().interactable = false;
+            this.GetComponent<Image>().sprite = null;
+            
+            _spritesCharacters._selectedCharacters[1].GetComponent<Sc_CanSelect>()._canSelect = true;
+            _spritesCharacters._selectedCharacters[1].GetComponent<Image>().color = Color.white;
+            _spritesCharacters._selectedCharacters[1] = null;    
+        }    
+    }
+
+    public void UndoThirdCharacter()
+    {
+        if(_spritesCharacters._selectedCharacters[2] != null)
+        {
+            this.GetComponentInChildren<Button>().interactable = true;
+            _spritesCharacters._selectCharacterButton[0].interactable = false;
+            _spritesCharacters._selectCharacterButton[1].interactable = false;
+            this.gameObject.transform.GetChild(0).GetChild(1).GetComponent<Button>().interactable = false;
+            this.GetComponent<Image>().sprite = null;
+
+            _spritesCharacters._selectedCharacters[2].GetComponent<Sc_CanSelect>()._canSelect = true;
+            _spritesCharacters._selectedCharacters[2].GetComponent<Image>().color = Color.white;
+            _spritesCharacters._selectedCharacters[2] = null;     
+        }
+    }
+
     public void SelectCharacters()
     {
-        if (Sc_SpritesCharacters.Instance._selectedCharacter.GetComponent<Sc_CanSelect>()._canSelect == true)
+        if (_spritesCharacters._characterToSelect.GetComponent<Sc_CanSelect>()._canSelect == true)
         {
-            this.GetComponent<Image>().sprite = Sc_SpritesCharacters.Instance._selectedCharacter.GetComponent<Image>().sprite;
-            Sc_SpritesCharacters.Instance._selectedCharacter.GetComponent<Image>().color = _characterAlreadySelectedColor;
-            Sc_SpritesCharacters.Instance._selectedCharacter.GetComponent<Sc_CanSelect>()._canSelect = false;
+            this.GetComponent<Image>().sprite = _spritesCharacters._characterToSelect.GetComponent<Image>().sprite;
+            _spritesCharacters._characterToSelect.GetComponent<Image>().color = _characterAlreadySelectedColor;
+            _spritesCharacters._characterToSelect.GetComponent<Sc_CanSelect>()._canSelect = false;
 
-            if (Sc_SpritesCharacters.Instance._selectButton[0].interactable == true)
+            if (_spritesCharacters._selectCharacterButton[0].interactable == true)
             {
-                Sc_SpritesCharacters.Instance._selectButton[0].interactable = false;
-                Sc_SpritesCharacters.Instance._selectButton[1].interactable = true;
-                Sc_SpritesCharacters.Instance._selectButton[2].interactable = false;
+                if (_firstSelection == false)
+                {
+                    _spritesCharacters._selectCharacterButton[1].interactable = true;
+                    _firstSelection = true;
+                }
+                
+                _spritesCharacters._selectCharacterButton[0].interactable = false;
+                _spritesCharacters._selectCharacterButton[2].interactable = false;
+                
+                _spritesCharacters._undoCharacterButton[0].interactable = true;
+                
+                _spritesCharacters._selectedCharacters[0] = _spritesCharacters._characterToSelect;
             }
-            else if (Sc_SpritesCharacters.Instance._selectButton[1].interactable == true)
+            else if (_spritesCharacters._selectCharacterButton[1].interactable == true)
             {
-                Sc_SpritesCharacters.Instance._selectButton[0].interactable = false;
-                Sc_SpritesCharacters.Instance._selectButton[1].interactable = false;
-                Sc_SpritesCharacters.Instance._selectButton[2].interactable = true;   
+                if (_secondSelection == false)
+                {
+                    _spritesCharacters._selectCharacterButton[2].interactable = true;
+                    _secondSelection = true;
+                }
+                _spritesCharacters._selectCharacterButton[0].interactable = false;
+                _spritesCharacters._selectCharacterButton[1].interactable = false;
+                
+                _spritesCharacters._undoCharacterButton[1].interactable = true;
+                
+                _spritesCharacters._selectedCharacters[1] = _spritesCharacters._characterToSelect;
             }
             else
             {
-                Sc_SpritesCharacters.Instance._selectButton[0].interactable = false;
-                Sc_SpritesCharacters.Instance._selectButton[1].interactable = false;
-                Sc_SpritesCharacters.Instance._selectButton[2].interactable = false;  
+                if (_thirdSelection == false)
+                {
+                    _thirdSelection = true;
+                }
+                _spritesCharacters._selectCharacterButton[2].interactable = false;
+                _spritesCharacters._selectCharacterButton[0].interactable = false;
+                _spritesCharacters._selectCharacterButton[1].interactable = false;
+                
+                _spritesCharacters._undoCharacterButton[2].interactable = true;
+                
+                _spritesCharacters._selectedCharacters[2] = _spritesCharacters._characterToSelect;
             }
         }
     }
--- Gold_Field_Carriers/Assets/Scripts/UI/Sc_SpritesCharacters.cs
+++ Gold_Field_Carriers/Assets/Scripts/UI/Sc_SpritesCharacters.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,10 +8,12 @@ public class Sc_SpritesCharacters : MonoBehaviour
 {
     [Header("Characters")]
     public List<GameObject> _charactersSelection = new List<GameObject>();
-    public GameObject _selectedCharacter;
+    public List<GameObject> _selectedCharacters = new List<GameObject>(3);
+    public GameObject _characterToSelect;
 
     [Header("Button")] 
-    public List<Button> _selectButton = new List<Button>();
+    public List<Button> _selectCharacterButton = new List<Button>();
+    public List<Button> _undoCharacterButton = new List<Button>();
     
     [SerializeField] private int _characterIndex = 2;
     
@@ -31,24 +34,48 @@ public class Sc_SpritesCharacters : MonoBehaviour
 
     private void Start()
     {
-        _selectedCharacter = _charactersSelection[_characterIndex];
-        _selectButton[1].interactable = false;
-        _selectButton[2].interactable = false;
+        _characterToSelect = _charactersSelection[_characterIndex];
+        _selectCharacterButton[1].interactable = false;
+        _selectCharacterButton[2].interactable = false;
+
+        foreach (var button in _undoCharacterButton)
+        {
+            button.interactable = false;
+        }
     }
 
     public void RightArrow()
     {
         Vector3 pos = _charactersSelection[0].GetComponent<RectTransform>().position;
         Vector2 size = _charactersSelection[0].GetComponent<RectTransform>().sizeDelta;
+
+        Vector2 textsize = _charactersSelection[0].transform.GetChild(0).GetComponent<RectTransform>().sizeDelta;
+        Vector3 textpos = _charactersSelection[0].transform.GetChild(0).GetComponent<RectTransform>().position;
+        float fontsize = _charactersSelection[0].transform.GetChild(0).GetComponent<TMP_Text>().fontSize;
         
         for (var i = _charactersSelection.Count -1; i >= 0; i--)
         {
             Vector3 pos2 = _charactersSelection[i].GetComponent<RectTransform>().position;
             Vector2 size2 = _charactersSelection[i].GetComponent<RectTransform>().sizeDelta;
+            
+            Vector2 textsize2 = _charactersSelection[i].transform.GetChild(0).GetComponent<RectTransform>().sizeDelta;
+            Vector3 textpos2 = _charactersSelection[i].transform.GetChild(0).GetComponent<RectTransform>().position;
+            float fontsize2 = _charactersSelection[i].transform.GetChild(0).GetComponent<TMP_Text>().fontSize;
+            
             _charactersSelection[i].GetComponent<RectTransform>().position = pos;
             _charactersSelection[i].GetComponent<RectTransform>().sizeDelta = size;
+            
+            _charactersSelection[i].transform.GetChild(0).GetComponent<RectTransform>().sizeDelta = textsize;
+            _charactersSelection[i].transform.GetChild(0).GetComponent<RectTransform>().position = textpos;
+            _charactersSelection[i].transform.GetChild(0).GetComponent<TMP_Text>().fontSize = fontsize;
+            
+            
             pos = pos2;
             size = size2;
+            
+            textsize = textsize2;
+            textpos = textpos2;
+            fontsize = fontsize2;
         }
 
         _characterIndex--;
@@ -56,7 +83,7 @@ public class Sc_SpritesCharacters : MonoBehaviour
         if (_characterIndex == -1)
             _characterIndex = 5;
         
-        _selectedCharacter = _charactersSelection[_characterIndex];
+        _characterToSelect = _charactersSelection[_characterIndex];
     }
 
     public void LeftArrow()
@@ -64,21 +91,38 @@ public class Sc_SpritesCharacters : MonoBehaviour
         Vector3 pos = _charactersSelection[_charactersSelection.Count - 1].GetComponent<RectTransform>().position;
         Vector2 size = _charactersSelection[_charactersSelection.Count - 1].GetComponent<RectTransform>().sizeDelta;
         
+        Vector2 textsize = _charactersSelection[_charactersSelection.Count - 1].transform.GetChild(0).GetComponent<RectTransform>().sizeDelta;
+        Vector3 textpos = _charactersSelection[_charactersSelection.Count - 1].transform.GetChild(0).GetComponent<RectTransform>().position;
+        float fontsize = _charactersSelection[_charactersSelection.Count - 1].transform.GetChild(0).GetComponent<TMP_Text>().fontSize;
+        
         for (var i = 0; i < _charactersSelection.Count; i++)
         {
             Vector3 pos2 = _charactersSelection[i].GetComponent<RectTransform>().position;
             Vector2 size2 = _charactersSelection[i].GetComponent<RectTransform>().sizeDelta;
+            
+            Vector2 textsize2 = _charactersSelection[i].transform.GetChild(0).GetComponent<RectTransform>().sizeDelta;
+            Vector3 textpos2 = _charactersSelection[i].transform.GetChild(0).GetComponent<RectTransform>().position;
+            float fontsize2 = _charactersSelection[i].transform.GetChild(0).GetComponent<TMP_Text>().fontSize;
+            
             _charactersSelection[i].GetComponent<RectTransform>().position = pos;
             _charactersSelection[i].GetComponent<RectTransform>().sizeDelta = size;
+            
+            _charactersSelection[i].transform.GetChild(0).GetComponent<RectTransform>().sizeDelta = textsize;
+            _charactersSelection[i].transform.GetChild(0).GetComponent<RectTransform>().position = textpos;
+            _charactersSelection[i].transform.GetChild(0).GetComponent<TMP_Text>().fontSize = fontsize;
+            
             pos = pos2;
             size = size2;
+            
+            textsize = textsize2;
+            textpos = textpos2;
+            fontsize = fontsize2;
         }
         _characterIndex++;
 
         if (_characterIndex == 6)
             _characterIndex = 0;
         
-        _selectedCharacter = _charactersSelection[_characterIndex];
-        
+        _characterToSelect = _charactersSelection[_characterIndex];
     }
 }
