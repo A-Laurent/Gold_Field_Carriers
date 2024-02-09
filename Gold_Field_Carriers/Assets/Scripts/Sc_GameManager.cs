6f82edce2b93f7fae8becfe505344d2ec86c7a91
Nova3324 <118186038+Nova3324@users.noreply.github.com>
2024-02-09 17:05:31 +0100

    Map Decoration
    - add decoration in the map
    - animation for decoration

--- Gold_Field_Carriers/Assets/Scripts/Sc_GameManager.cs
+++ Gold_Field_Carriers/Assets/Scripts/Sc_GameManager.cs
@@ -4,13 +4,39 @@ using UnityEngine;
 
 public class Sc_GameManager : MonoBehaviour
 {
+    [SerializeField] private List<GameObject> _riverDecoration = new List<GameObject>();
+    [SerializeField] private List<GameObject> _desertDecoration = new List<GameObject>();
+    
     void Start()
     {
- 
+        RaiseDecoration(_riverDecoration);
+        StartCoroutine(Raise());
     }
     
     void Update()
     {
         
+    }
+
+    public void RaiseDecoration(List<GameObject> zonedecoration)
+    {
+        foreach (var decoration in zonedecoration)
+        {
+            decoration.GetComponent<Animation>().Play("Raise");
+        }
+    }
+    
+    public void LowerDecoration(List<GameObject> zonedecoration)
+    {
+        foreach (var decoration in zonedecoration)
+        {
+            decoration.GetComponent<Animation>().Play("Lower");
+        }
+    }
+
+    IEnumerator Raise()
+    {
+        yield return new WaitForSeconds(3f);
+        LowerDecoration(_riverDecoration);
     }
 }
